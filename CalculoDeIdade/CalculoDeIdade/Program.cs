25a71027e7f0a0b5b0a57ca9b795193a208306f7
MarcioFeZa <marciofeza@hotmail.com>
2025-03-26 10:59:21 -0300

    Calculo de Idade
    Primeiro programa em C#

---
+++ CalculoDeIdade/CalculoDeIdade/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CalculoDeIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ano_nascimento, ano_atual, idade;
+
+            Console.Write("Digite o ano de seu nascimento: ");  //Console.Write = escreva no portugol
+            ano_nascimento = int.Parse(Console.ReadLine());     //int.Parse = converte texto em número
+            Console.Write("Digite o ano atual: ");
+            ano_atual = int.Parse(Console.ReadLine());
+            idade = ano_atual - ano_nascimento;
+            Console.Write("sua idade é: " + idade + " anos");
+
+
+
+        }
+    }
+}
---
+++ CalculoDeIdade/CalculoDeIdade/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("CalculoDeIdade")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculoDeIdade")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("49982516-0f39-455d-a4cd-e336c701cb20")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
