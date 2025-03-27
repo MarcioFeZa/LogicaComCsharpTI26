3dac9277fd44339cb13f506095085cf1c9dd4780
MarcioFeZa <marciofeza@hotmail.com>
2025-03-27 09:49:21 -0300

    NumeroParAte100

---
+++ NumeroParAte100/NumeroParAte100/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroParAte100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mostre somente os números pares de 1 a 100
+
+            int contador;
+
+
+            for(contador = 2; contador <= 100; contador++) //for é a mesma coisa que (para) no portugol
+            {
+            if(contador % 2 == 0)
+            {
+                    Console.WriteLine(contador + "\n");
+            }
+
+            }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ NumeroParEnquanto/NumeroParEnquanto/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroParEnquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contadorPares = 0;
+
+
+            while(contadorPares <= 100)//while mesma função que enquanto no portugol
+            {
+                if(contadorPares % 2 == 0)
+            {
+                    Console.WriteLine(contadorPares + "\n");
+            }
+                contadorPares++;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
