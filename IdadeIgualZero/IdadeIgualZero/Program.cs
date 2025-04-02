df25a8a5d9f07e21776bd2ee6aabb955e7c3cfb9
MarcioFeZa <marciofeza@hotmail.com>
2025-04-02 09:25:28 -0300

    IdadeIgaulZero

---
+++ IdadeIgualZero/IdadeIgualZero/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdadeIgualZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, anoDeNasc, contador = 1;
+
+
+            while (contador > 0)
+            {
+                Console.WriteLine("Digite a sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o ano de nascimento: ");
+                anoDeNasc = int.Parse(Console.ReadLine());
+
+
+                Console.Clear();
+
+
+                if (idade == 0)
+                {
+                    Console.WriteLine("Fim do programa, idade 0");
+                    break;
+                }
+            }
+
+        }
+    }
+}
