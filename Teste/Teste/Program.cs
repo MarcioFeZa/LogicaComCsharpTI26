f7305b088de91973e390fe69e5cc158e8d8b2c90
MarcioFeZa <marciofeza@hotmail.com>
2025-04-04 11:56:04 -0300

    04/04

---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reajuste5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Reajuste de 5%";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("╔═══════════════════════════════════════════════════════════════════════╗");
+            Console.Write(@"
+°
+°
+°
+°
+");
+
+            Console.WriteLine("╚═══════════════════════════════════════════════════════════════════════╝");
+            Console.WriteLine("Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%.");
+            double valor, valorReajustado;
+
+            Console.Write("Digite um valor: ");
+            valor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor normal: " + valor);
+            valorReajustado = (valor * 0.05) + valor;
+
+            Console.WriteLine("Valor reajustado: " + valorReajustado);
+        }
+    }
+}
---
+++ ValoresAeB/ValoresAeB/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+           
+
+
+namespace ValorAeB
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                /*
+            Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais, deverá somar os dois valores, 
+             caso contrário devera multiplicar A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado a uma variável C e
+             imprimir seu valor na tela.
+             */
+                int a, b, c;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\r\n          _____                    _____                    _____           _______                   _____          \r\n         /\\    \\                  /\\    \\                  /\\    \\         /::\\    \\                 /\\    \\         \r\n        /::\\____\\                /::\\    \\                /::\\____\\       /::::\\    \\               /::\\    \\        \r\n       /:::/    /               /::::\\    \\              /:::/    /      /::::::\\    \\             /::::\\    \\       \r\n      /:::/    /               /::::::\\    \\            /:::/    /      /::::::::\\    \\           /::::::\\    \\      \r\n     /:::/    /               /:::/\\:::\\    \\          /:::/    /      /:::/~~\\:::\\    \\         /:::/\\:::\\    \\     \r\n    /:::/____/               /:::/__\\:::\\    \\        /:::/    /      /:::/    \\:::\\    \\       /:::/__\\:::\\    \\    \r\n    |::|    |               /::::\\   \\:::\\    \\      /:::/    /      /:::/    / \\:::\\    \\     /::::\\   \\:::\\    \\   \r\n    |::|    |     _____    /::::::\\   \\:::\\    \\    /:::/    /      /:::/____/   \\:::\\____\\   /::::::\\   \\:::\\    \\  \r\n    |::|    |    /\\    \\  /:::/\\:::\\   \\:::\\    \\  /:::/    /      |:::|    |     |:::|    | /:::/\\:::\\   \\:::\\____\\ \r\n    |::|    |   /::\\____\\/:::/  \\:::\\   \\:::\\____\\/:::/____/       |:::|____|     |:::|    |/:::/  \\:::\\   \\:::|    |\r\n    |::|    |  /:::/    /\\::/    \\:::\\  /:::/    /\\:::\\    \\        \\:::\\    \\   /:::/    / \\::/   |::::\\  /:::|____|\r\n    |::|    | /:::/    /  \\/____/ \\:::\\/:::/    /  \\:::\\    \\        \\:::\\    \\ /:::/    /   \\/____|:::::\\/:::/    / \r\n    |::|____|/:::/    /            \\::::::/    /    \\:::\\    \\        \\:::\\    /:::/    /          |:::::::::/    /  \r\n    |:::::::::::/    /              \\::::/    /      \\:::\\    \\        \\:::\\__/:::/    /           |::|\\::::/    /   \r\n    \\::::::::::/____/               /:::/    /        \\:::\\    \\        \\::::::::/    /            |::| \\::/____/    \r\n     ~~~~~~~~~~                    /:::/    /          \\:::\\    \\        \\::::::/    /             |::|  ~|          \r\n                                  /:::/    /            \\:::\\    \\        \\::::/    /              |::|   |          \r\n                                 /:::/    /              \\:::\\____\\        \\::/____/               \\::|   |          \r\n                                 \\::/    /                \\::/    /         ~~                      \\:|   |          \r\n                                  \\/____/                  \\/____/                                   \\|___|          \r\n                                                                                                                     \r\n");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Faça um programa que leia dois numeros inteiros");
+                Console.ResetColor();
+                Console.Write("Digite o primeiro numero: ");
+                a = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o segundo numero: ");
+                b = int.Parse(Console.ReadLine());
+
+                if (a == b)
+                {
+                    c = a + b;
+                    Console.WriteLine("Resultado: " + c);
+                }
+                else
+                {
+                    c = a * b;
+                    Console.WriteLine("Resultado: " + c);
+                }
+            }
+        }
+    }
+
+
+
+
+
+    
+
