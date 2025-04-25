e6bb360cf513e2721616e2c2dd839a654662a088
MarcioFeZa <marciofeza@hotmail.com>
2025-04-25 11:34:51 -0300

    array

--- ArrayContoleDeEstoque/ArrayContoleDeEstoque/Program.cs
+++ ArrayContoleDeEstoque/ArrayContoleDeEstoque/Program.cs
@@ -14,26 +14,43 @@ namespace ArrayContoleDeEstoque
             //"Implemente um sistema que armazene a quantidade de 50 produtos em estoque e informe
             //o produto com maior e menor quantidade disponível."
 
-            string[] produto = new string[50];
-            int[] quantidade = new int[50];
-            int[] estoque = new int[50];    
+                int[] produtos = new int[50];
 
-            for (int i = 0; i < 50; i++)
+                int quantidadeProduto;
 
-            {
+                for (int i = 0; i < produtos.Length; i++)
+                {
+                    Console.WriteLine($"Digite a quantidade em estoque do {i + 1}° Produto: ");
+                    quantidadeProduto = int.Parse(Console.ReadLine());
+                    produtos[i] = quantidadeProduto;
 
-                Console.WriteLine($"Digite o nome do produto: {i + 1}: ");
+                }
+                int Maior = produtos.Max();
+                int Menor = produtos.Min();
+                int posicaoMaior = 0;
+                int posicaoMenor = 0;
 
-                int quantidade = int.Parse(Console.ReadLine());
-                estoque($"Produto_{i}", quantidade);
+                for (int j = 0; j < produtos.Length; j++)
+                {
+                    if (produtos[j] == Maior)
+                    {
+                        posicaoMaior = j;
+                    }
+                    if (produtos[j] == Menor)
+                    {
+                        posicaoMenor = j;
+                    }
+                }
 
+                Console.WriteLine($"O Produto com maior quantidade é o produto {posicaoMaior + 1} Com a quantidade de {Maior} em estoque");
+                Console.WriteLine($"O Produto com menor quantidade em estoque  é o produto {posicaoMenor + 1}  Com a quantidade de {Menor} em estoque");
             }
 
-            
+
         }
     }
 
-}
+
 
     
 
